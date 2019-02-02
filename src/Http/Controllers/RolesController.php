4bd1d3d4b33eee6524cd70657532906e5061f486
Jose Galindo <jgalindo@softlogic.com.gt>
2019-02-01 19:04:01 -0600

    Roles fix

--- src/Http/Controllers/RolesController.php
+++ src/Http/Controllers/RolesController.php
@@ -1,153 +1,145 @@
 <?php
 namespace Csgt\Utils\Http\Controllers;
 
-use App\Models\Auth\Module;
-use App\Models\Auth\Role;
-use Cancerbero;
+use DB;
 use Crypt;
-use Csgt\Crud\CrudController;
+use Cancerbero;
+use App\Models\Auth\Role;
+use App\Models\Auth\Module;
 use Illuminate\Http\Request;
+use Csgt\Crud\CrudController;
+use App\Models\Auth\RoleModulePermission;
 
-class RolesController extends CrudController {
-	public function __construct() {
-		$this->setModel(new Role);
-		$this->setTitle('Roles');
+class RolesController extends CrudController
+{
+    public function __construct()
+    {
+        $this->setModel(new Role);
+        $this->setTitle('Roles');
+        $this->setField(['name' => 'Nombre', 'field' => 'name']);
+        $this->setField(['name' => 'Descripción', 'field' => 'description']);
+        $this->middleware(function ($request, $next) {
+            if (!Cancerbero::isGod()) {
+                $this->setWhere('id', '<>', Cancerbero::godRole());
+            }
 
-		$this->setField(['name' => 'Nombre', 'field' => 'name']);
-		$this->setField(['name' => 'Descripción', 'field' => 'description']);
+            return $next($request);
+        });
+        $this->setPermissions("\Cancerbero::crudPermissions", 'catalogs.roles');
+    }
 
-		$this->middleware(function ($request, $next) {
-			if (!Cancerbero::isGod()) {
-				$this->setWhere('id', '<>', Cancerbero::godRole());
-			}
+    public function detail(Request $request, $id)
+    {
+        $rmpids = [];
+        $role   = ['name' => null, 'description' => null];
 
-			return $next($request);
-		});
-		$this->setPermissions("\Cancerbero::crudPermissions", 'catalogs.roles');
-	}
+        if ($id !== '0') {
+            $id   = Crypt::decrypt($id);
+            $role = Role::with('role_module_permissions:id,role_id,module_permission_id')
+                ->findOrFail($id);
 
-	public function detail(Request $request, $id) {
-		$rmpids = [];
+            $rmpids = $role->role_module_permissions->map(function ($rmp) {
+                return $rmp->module_permission_id;
+            })->toArray();
+        }
 
-		if ($id != 0) {
-			$role = Role::with('role_module_permissions:id,role_id,module_permission_id')
-				->findOrFail($id);
+        $modules = Module::query()
+            ->with([
+                'modulepermissions',
+                'modulepermissions.permission' => function ($query) {
+                    $query->orderBy('name');
+                },
+            ])
+            ->orderBy('name')
+            ->get();
 
-			$rmpids = $role->role_module_permissions->map(function ($rmp) {
-				return $rmp->module_permission_id;
-			})->toArray();
-		}
+        $modules = $modules->map(function ($module) use ($rmpids) {
+            $mp = $module->modulepermissions->map(function ($mp) use ($rmpids) {
+                $mp->enabled = in_array($mp->id, $rmpids);
 
-		$modules = Module::query()
-			->with([
-				'modulepermissions',
-				'modulepermissions.permission' => function ($query) {
-					$query->orderBy('name');
-				},
-			])
-			->orderBy('name')
-			->get();
+                return $mp;
+            });
+            $module->modulepermissions = $mp;
 
-		$modules = $modules->map(function ($module) use ($rmpids) {
-			$mp = $module->modulepermissions->map(function ($mp) use ($rmpids) {
-				$mp->enabled = in_array($mp->id, $rmpids);
+            return $module;
+        });
 
-				return $mp;
-			});
-			$module->modulepermissions = $mp;
+        return response()->json([
+            'role'    => $role,
+            'modules' => $modules,
+        ]);
+    }
 
-			return $module;
-		});
+    public function create(Request $request)
+    {
+        return $this->edit($request, 0);
+    }
 
-		return response()->json([
-			'role' => $role,
-			'modules' => $modules,
-		]);
-	}
-
-	public function create(Request $request) {
-		return $this->edit($request, 0);
-	}
-
-	public function edit(Request $request, $id) {
-		$breadcrumb = '<ol class="breadcrumb float-sm-right">
+    public function edit(Request $request, $id)
+    {
+        $breadcrumb = '<ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item">Catálogos</li>
             <li class="breadcrumb-item"><a href="/catalogos/roles">Roles</a></li>
             <li class="breadcrumb-item active">Rol</li>
         </ol>';
 
-		return view('component')
-			->withTitle($this->getTitle())
-			->withBreadcrumb($breadcrumb)
-			->with('component', 'catalogs-roles-edit');
+        $params = ['id' => $id];
 
-		// if ($id !== 0) {
-		//     $id = Crypt::decrypt($id);
-		// }
+        return view('component')
+            ->withTitle($this->getTitle())
+            ->withBreadcrumb($breadcrumb)
+            ->with('params', $params)
+            ->with('component', 'catalogs-roles-edit');
+    }
 
-		// $roleName = 'Nuevo';
+    public function store(Request $request)
+    {
+        return $this->update($request, 0);
+    }
 
-		// // $modulePermissions = Module::with(['permissions',
-		// //     'role_module_permission' => function ($query) use ($id) {
-		// //         return $query->where('role_id', $id);
-		// //     },
-		// // ])
-		// //     ->orderBy('name')
-		// //     ->get();
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'role.name'        => 'required',
+            'role.description' => 'required',
+        ];
 
-		// $role = Role::find($id);
+        $messages = [
+            'role.name.required'        => 'El nombre es requerido',
+            'role.description.required' => 'La descripción es requerida',
+        ];
 
-		// if ($id !== 0) {
-		//     $roleName = $role->name;
-		// }
-		// $breadcrumb = '<ol class="breadcrumb float-sm-right">
-		//     <li class="breadcrumb-item">Catálogos</li>
-		//     <li class="breadcrumb-item"><a href="/catalogos/roles">Roles</a></li>
-		//     <li class="breadcrumb-item active">' . $roleName . '</li>
-		// </ol>';
+        $request->validate($rules, $messages);
 
-		// return view('catalogs.roles.edit')
-		//     ->withData($role)
-		//     ->withTitle($this->getTitle())
-		//     ->withBreadcrumb($breadcrumb)
-		//     ->withTemplate($this->getLayout())
-		//     ->withId(($id == 0 ? 0 : Crypt::encrypt($id)));
-		// ->withModulePermission($modulePermissions);
-	}
+        DB::transaction(function () use ($request, $id) {
 
-	public function store(Request $request) {
-		return $this->update($request, 0);
-	}
+            if ($id !== 0) {
+                $roleid = Crypt::decrypt($id);
+                $role   = Role::findOrFail($roleid);
 
-	public function update(Request $request, $id) {
-		if ($id !== 0) {
-			$rolid = Crypt::decrypt($request->id);
-			$rol = Authrol::find($rolid);
-			$rol->nombre = $request->nombre;
-			$rol->descripcion = $request->descripcion;
-			$rol->save();
-			Authrolmodulopermiso::where('rolid', $rolid)->delete();
-		} else {
-			$rol = new Authrol;
-			$rol->nombre = $request->nombre;
-			$rol->descripcion = $request->descripcion;
-			$rol->save();
-			$rolid = $rol->rolid;
-		}
+                //Authrolmodulopermiso::where('roleid', $roleid)->delete();
+            } else {
+                $role = new Role;
+            }
 
-		$modulopermisos = $request->modulopermisos;
+            $role->name        = $request->role['name'];
+            $role->description = $request->role['description'];
+            $role->save();
 
-		if ($modulopermisos) {
-			foreach ($modulopermisos as $modulopermiso) {
-				$authmodulopermiso = new Authrolmodulopermiso;
-				$authmodulopermiso->rolid = $rolid;
-				$authmodulopermiso->modulopermisoid = $modulopermiso;
-				$authmodulopermiso->save();
-			}
-		}
+            RoleModulePermission::where('role_id', $role->id)->delete();
 
-		return redirect()->route('catalogos.roles.index')
-			->with('flashMessage', config('cancerbero::mensajerolmodulopermisoexitoso'))
-			->with('flashType', 'success');
-	}
+            foreach ($request->modules as $module) {
+                foreach ($module['modulepermissions'] as $mp) {
+                    if ($mp['enabled']) {
+                        $rmp                       = new RoleModulePermission;
+                        $rmp->role_id              = $role->id;
+                        $rmp->module_permission_id = $mp['id'];
+                        $rmp->save();
+                    }
+                }
+            }
+        });
+
+        return response()->json('ok');
+    }
 }
--- src/resources/views/catalogs/RolesEdit.vue
+++ src/resources/views/catalogs/RolesEdit.vue
@@ -1,52 +1,88 @@
 <template>
-
-        <div class="card">
-
+    <div class="card">
+        <div v-if="loading" class="text-center">
             <div class="card-body">
-                <div class="row">
-                    <InputField label="Nombre" :value="data.role.name" />
-                    <div class="form-group col-sm-12">
-                        <label for="nombre">Nombre</label>
-                        <input name="nombre" type="text" class="form-control" v-model="data.role.name">
-                    </div>
-                    <div class="form-group col-sm-12">
-                        <label for="descripcion">Descripción</label>
-                        <input name="descripcion" type="text" class="form-control" v-model="data.role.description">
-                    </div>
-                </div>
-                <label>Permisos</label>
-                <div class="row">
-                    <div v-for="m in data.modules" class="col-sm-4">
-                        <catalogs-rolemodule :module="m"/>
-                    </div>
-                </div>
-            </div>
-            <div class="card-footer">
-                <button class="btn btn-primary">Guardar</button>
+                <i class="fas fa-spinner fa-spin fa-2x"></i>
             </div>
         </div>
+        <template v-else>
+            <div class="card-body">
+                <template>
+                    <div class="row">
+                        <div class="form-group col-sm-12">
+                            <label for="nombre">Nombre</label>
+                            <input name="nombre" type="text" class="form-control" v-model="data.role.name">
+                        </div>
+                        <div class="form-group col-sm-12">
+                            <label for="descripcion">Descripción</label>
+                            <input name="descripcion" type="text" class="form-control" v-model="data.role.description">
+                        </div>
+                    </div>
+                    <label>Permisos</label>
+                    <div class="row">
+                        <div v-for="m in data.modules" class="col-sm-4">
+                            <catalogs-rolemodule :module="m"/>
+                        </div>
+                    </div>
+                </template>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-primary" @click="save" :disabled="saving">Guardar</button>
+            </div>
+        </template>
+    </div>
 </template>
 <script>
     import axios from 'axios';
-
     export default {
         data() {
             return {
                 data: {
-                    role: {
-                        name: null,
-                        description: null,
-                    },
-                    modules: []
+                    role: null,
+                    modules: [],
                 },
+                loading : true,
+                saving: false
             }
         },
+        props: ['id'],
         mounted() {
-            axios.get('/catalogs/roles/1/detail')
+            axios.get('/catalogs/roles/' + this.id + '/detail')
             .then(response => {
-
+                this.loading = false
                 this.data = response.data
+            })
+            .catch((e) => {
+                this.loading = false
+                alert(e);
             });
+        },
+        methods: {
+            save() {
+                this.saving = true
+                if (this.id != 0) {
+                    axios.patch('/catalogs/roles/' + this.id, this.data)
+                        .then((response)=> {
+                            window.location = '/catalogs/roles'
+                        })
+                        .catch((error) => {
+                            this.saving = false
+                            alert(error.response.data.message)
+                        })
+                }
+                else {
+                   axios.post('/catalogs/roles', this.data)
+                        .then((response)=> {
+                            window.location = '/catalogs/roles'
+                        })
+                        .catch((error) => {
+                            this.saving = false
+                            alert(error.response.data.message)
+                        })
+                }
+
+
+            },
         }
     }
 </script>
