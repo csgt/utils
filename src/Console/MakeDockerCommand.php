51ddb65eea1abd35edfdad91e8baaf357c763a19
Jose Galindo <jgalindo@softlogic.com.gt>
2019-08-27 11:39:53 -0600

    Ordenar y agregar dockers

--- src/Console/MakeDockerCommand.php
+++ src/Console/MakeDockerCommand.php
@@ -11,13 +11,19 @@ class MakeDockerCommand extends Command
         'dockerfiles',
     ];
     protected $files = [
-        'docker/docker-compose.yml.example.stub' => 'docker-compose.yml.example',
-        'docker/docker-compose.yml.stub'         => 'docker-compose.yml',
-        'docker/dockerfiles/mysql.docker.stub'   => 'dockerfiles/mysql.docker',
-        'docker/dockerfiles/php.docker.stub'     => 'dockerfiles/php.docker',
-        'docker/dockerfiles/nginx.docker.stub'   => 'dockerfiles/nginx.docker',
-        'docker/dockerfiles/vhost.conf.stub'     => 'dockerfiles/vhost.conf',
-        'docker/dockerfiles/my.cnf.stub'         => 'dockerfiles/my.cnf',
+        'docker/docker-compose.yml.example.stub'             => 'docker-compose.yml.example',
+        'docker/docker-compose.yml.stub'                     => 'docker-compose.yml',
+        'docker/dockerfiles/horizon/horizon.docker.stub'     => 'dockerfiles/horizon/horizon.docker',
+        'docker/dockerfiles/horizon/horizon.sh.stub'         => 'dockerfiles/horizon/horizon.sh',
+        'docker/dockerfiles/mysql/mysql.docker.stub'         => 'dockerfiles/mysql/mysql.docker',
+        'docker/dockerfiles/mysql/my.cnf.stub'               => 'dockerfiles/mysql/my.cnf',
+        'docker/dockerfiles/nginx/nginx.docker.stub'         => 'dockerfiles/nginx/nginx.docker',
+        'docker/dockerfiles/nginx/vhost.conf.stub'           => 'dockerfiles/nginx/vhost.conf',
+        'docker/dockerfiles/php/php.docker.stub'             => 'dockerfiles/php/php.docker',
+        'docker/dockerfiles/php/limits.conf.stub'            => 'dockerfiles/php/limits.conf',
+        'docker/dockerfiles/redis/redis.docker.stub'         => 'dockerfiles/redis/redis.docker',
+        'docker/dockerfiles/scheduler/scheduler.docker.stub' => 'dockerfiles/scheduler/scheduler.docker',
+        'docker/dockerfiles/scheduler/scheduler.sh.stub'     => 'dockerfiles/scheduler/scheduler.sh',
     ];
 
     public function handle()
