67ca9f1c32859b645bea9107491aa502a95b716c
Jose Galindo <jgalindo@softlogic.com.gt>
2020-07-02 20:35:21 -0600

    Eliminar langs, docker extras, controllers (se movieron a UI), limpieza de metodos

--- src/Console/MakeDockerCommand.php
+++ src/Console/MakeDockerCommand.php
@@ -9,29 +9,22 @@ class MakeDockerCommand extends Command
     protected $description = 'Create docker configurations';
     protected $directories = [
         'dockerfiles',
-        'dockerfiles/horizon',
         'dockerfiles/mysql',
         'dockerfiles/nginx',
         'dockerfiles/php',
-        'dockerfiles/redis',
-        'dockerfiles/scheduler',
     ];
     protected $files = [
-        'docker/docker-compose.yml.example.stub'             => 'docker-compose.yml.example',
-        'docker/docker-compose.yml.stub'                     => 'docker-compose.yml',
-        'docker/dockerfiles/horizon/horizon.docker.stub'     => 'dockerfiles/horizon/horizon.docker',
-        'docker/dockerfiles/horizon/horizon.sh.stub'         => 'dockerfiles/horizon/horizon.sh',
-        'docker/dockerfiles/mysql/mysql.docker.stub'         => 'dockerfiles/mysql/mysql.docker',
-        'docker/dockerfiles/mysql/my.cnf.stub'               => 'dockerfiles/mysql/my.cnf',
-        'docker/dockerfiles/nginx/nginx.docker.stub'         => 'dockerfiles/nginx/nginx.docker',
-        'docker/dockerfiles/nginx/vhost.conf.stub'           => 'dockerfiles/nginx/vhost.conf',
-        'docker/dockerfiles/nginx/gzip.conf.stub'            => 'dockerfiles/nginx/gzip.conf',
-        'docker/dockerfiles/php/php.docker.stub'             => 'dockerfiles/php/php.docker',
-        'docker/dockerfiles/php/limits.conf.stub'            => 'dockerfiles/php/limits.conf',
-        'docker/dockerfiles/php/opcache.conf.stub'           => 'dockerfiles/php/opcache.conf',
-        'docker/dockerfiles/redis/redis.docker.stub'         => 'dockerfiles/redis/redis.docker',
-        'docker/dockerfiles/scheduler/scheduler.docker.stub' => 'dockerfiles/scheduler/scheduler.docker',
-        'docker/dockerfiles/scheduler/scheduler.sh.stub'     => 'dockerfiles/scheduler/scheduler.sh',
+        'docker/docker-compose.yml.example.stub'     => 'docker-compose.yml.example',
+        'docker/docker-compose.yml.stub'             => 'docker-compose.yml',
+        'docker/dockerfiles/mysql/mysql.docker.stub' => 'dockerfiles/mysql/mysql.docker',
+        'docker/dockerfiles/mysql/my.cnf.stub'       => 'dockerfiles/mysql/my.cnf',
+        'docker/dockerfiles/nginx/nginx.docker.stub' => 'dockerfiles/nginx/nginx.docker',
+        'docker/dockerfiles/nginx/vhost.conf.stub'   => 'dockerfiles/nginx/vhost.conf',
+        'docker/dockerfiles/nginx/gzip.conf.stub'    => 'dockerfiles/nginx/gzip.conf',
+        'docker/dockerfiles/php/php.docker.stub'     => 'dockerfiles/php/php.docker',
+        'docker/dockerfiles/php/limits.conf.stub'    => 'dockerfiles/php/limits.conf',
+        'docker/dockerfiles/php/php.sh.stub'         => 'dockerfiles/php/php.sh',
+        'docker/dockerfiles/php/opcache.conf.stub'   => 'dockerfiles/php/opcache.conf',
     ];
 
     public function handle()
--- src/Console/MakeUtilsCommand.php
+++ src/Console/MakeUtilsCommand.php
@@ -11,59 +11,17 @@ class MakeUtilsCommand extends Command
 
     protected $views = [];
 
-    protected $controllers = [
-        'Catalogs/RolesController',
-        'Catalogs/UsersController',
-    ];
-
     protected $models = [
         'Menu' => 'Menu',
-    ];
-
-    protected $langs = [
-        'es/usuario.stub' => 'es/usuario.php',
-        'en/usuario.stub' => 'en/usuario.php',
     ];
 
     public function handle()
     {
         $this->createDirectories();
-        $this->exportControllers();
         $this->exportModels();
-        $this->exportViews();
         $this->exportLangs();
 
         $this->info('CSGT Views and controllers generated correctly.');
-    }
-
-    protected function exportControllers()
-    {
-        foreach ($this->controllers as $controller) {
-            file_put_contents(
-                app_path('Http/Controllers/' . $controller . '.php'),
-                $this->compileControllerStub($controller)
-            );
-        }
-    }
-
-    protected function exportViews()
-    {
-        foreach ($this->views as $key => $value) {
-            copy(
-                __DIR__ . '/stubs/make/views/' . $key,
-                base_path('resources/views/' . $value)
-            );
-        }
-    }
-
-    protected function exportLangs()
-    {
-        foreach ($this->langs as $key => $value) {
-            copy(
-                __DIR__ . '/stubs/make/lang/' . $key,
-                base_path('resources/lang/' . $value)
-            );
-        }
     }
 
     protected function exportModels()
@@ -82,30 +40,9 @@ class MakeUtilsCommand extends Command
             mkdir(app_path('Http/Controllers/Catalogs'), 0755, true);
         }
 
-        if (!is_dir(resource_path('views/catalogs/roles'))) {
-            mkdir(resource_path('views/catalogs/roles'), 0755, true);
-        }
-
-        if (!is_dir(resource_path('views/catalogs/users'))) {
-            mkdir(resource_path('views/catalogs/users'), 0755, true);
-        }
-
         if (!is_dir(app_path('Models/Menu'))) {
             mkdir(app_path('Models/Menu'), 0755, true);
         }
-
-        if (!is_dir(base_path('routes/core'))) {
-            mkdir(base_path('routes/core'), 0755, true);
-        }
-    }
-
-    protected function compileControllerStub($aPath, $aExtension = "stub")
-    {
-        return str_replace(
-            '{{namespace}}',
-            $this->getAppNamespace(),
-            file_get_contents(__DIR__ . '/stubs/make/controllers/' . $aPath . '.' . $aExtension)
-        );
     }
 
     protected function compileModelStub($aModel, $aExtension = "stub")
@@ -119,6 +56,6 @@ class MakeUtilsCommand extends Command
 
     protected function getAppNamespace()
     {
-        return 'App\\';
+        return $this->laravel->getNamespace();
     }
 }
