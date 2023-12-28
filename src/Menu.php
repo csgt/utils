ca585bdb6f3f7c0ad4bd471672cb20a16cc589b5
Jose Galindo <66330140+jgalindosl@users.noreply.github.com>
2023-12-28 08:30:27 -0600

    Backwards version with spanish table name and fields

--- src/Menu.php
+++ src/Menu.php
@@ -2,10 +2,9 @@
 namespace Csgt\Utils;
 
 use DB;
-use Cache;
 use Request;
 use Csgt\Menu\Menu as MenuC;
-use App\Models\Menu\Menu as MMenu;
+use App\Models\Menu\Authmenu as MMenu;
 
 class Menu
 {
@@ -25,61 +24,60 @@ class Menu
 
         $userRoles = auth()->user()->roleIds();
 
-        $menus = MMenu::select('parent_id', 'id')->get();
+        $menus = MMenu::select('padreid', 'menuid')->get();
         //Guardamos un array de parents para solo abrir el dataset una vez
         foreach ($menus as $menu) {
-            self::$parents[$menu->id] = (int) $menu->parent_id;
+            self::$parents[$menu->menuid] = (int) $menu->padreid;
         }
 
         //Buscamos todos los permisos (sin parents) y agregamos los parents
-        $permissions = MMenu::select('menus.id', DB::raw('coalesce(menus.parent_id,0) AS parent_id'))
-            ->leftJoin('role_module_permissions AS rmp', 'rmp.module_permission_id', '=', 'menus.module_permission_id')
-            ->leftJoin('module_permissions AS mp', 'mp.id', '=', 'rmp.module_permission_id')
-            ->leftJoin('modules AS mo', 'mo.id', '=', 'mp.module_id')
-            ->leftJoin('permissions AS p', 'p.id', '=', 'mp.permission_id')
-            ->whereIn('rmp.role_id', $userRoles)
+        $permissions = MMenu::select('authmenu.menuid', DB::raw('coalesce(authmenu.padreid,0) AS padreid'))
+            ->leftJoin('authrolmodulopermisos AS rmp', 'rmp.modulopermisoid', '=', 'authmenu.modulopermisoid')
+            ->leftJoin('authmodulopermisos AS mp', 'mp.modulopermisoid', '=', 'rmp.modulopermisoid')
+            ->leftJoin('authmodulos AS mo', 'mo.moduloid', '=', 'mp.moduloid')
+            ->leftJoin('authpermisos AS p', 'p.permisoid', '=', 'mp.permisoid')
+            ->whereIn('rmp.rolid', $userRoles)
             ->get();
 
         foreach ($permissions as $menu) {
-            self::$menuIds[] = $menu->id;
-            if ($menu->parent_id != 0) {
-                self::$menuIds[] = $menu->parent_id;
-                self::getParents($menu->parent_id);
+            self::$menuIds[] = $menu->menuid;
+            if ($menu->padreid != 0) {
+                self::$menuIds[] = $menu->padreid;
+                self::getParents($menu->padreid);
             }
         }
 
         //Ahora que ya tenemos todos los menuids que necesitamos, hacemos de nuevo el select IN
-        $arr         = [];
-        $permissions = MMenu::select('menus.name', DB::raw("CONCAT(mo.name,'.',p.name) AS route"),
-            'menus.parent_id', 'menus.id', 'menus.icon')
-            ->leftJoin('role_module_permissions AS rmp', 'rmp.module_permission_id', '=', 'menus.module_permission_id')
-            ->leftJoin('module_permissions AS mp', 'mp.id', '=', 'rmp.module_permission_id')
-            ->leftJoin('modules AS mo', 'mo.id', '=', 'mp.module_id')
-            ->leftJoin('permissions AS p', 'p.id', '=', 'mp.permission_id')
-            ->whereIn('menus.id', self::$menuIds)
-            ->orderBy('menus.parent_id')
-            ->orderBy('menus.order')
+        $permissions = MMenu::select('authmenu.nombre', DB::raw("CONCAT(mo.nombre,'.',p.nombre) AS ruta"),
+            'authmenu.padreid', 'authmenu.menuid', 'authmenu.icono')
+            ->leftJoin('authrolmodulopermisos AS rmp', 'rmp.modulopermisoid', '=', 'authmenu.modulopermisoid')
+            ->leftJoin('authmodulopermisos AS mp', 'mp.modulopermisoid', '=', 'rmp.modulopermisoid')
+            ->leftJoin('authmodulos AS mo', 'mo.moduloid', '=', 'mp.moduloid')
+            ->leftJoin('authpermisos AS p', 'p.permisoid', '=', 'mp.permisoid')
+            ->whereIn('authmenu.menuid', self::$menuIds)
+            ->orderBy('authmenu.padreid')
+            ->orderBy('authmenu.orden')
             ->get()
             ->map(function ($menu) {
                 return [
-                    'name'      => $menu->name,
-                    'route'     => $menu->route,
-                    'icon'      => $menu->icon,
-                    'parent_id' => (int) $menu->parent_id,
-                    'id'        => $menu->id,
+                    'nombre'  => $menu->nombre,
+                    'ruta'    => $menu->ruta,
+                    'icono'   => $menu->icono,
+                    'padreid' => (int) $menu->padreid,
+                    'menuid'  => $menu->menuid,
                 ];
             });
 
         return collect($permissions->unique());
     }
 
-    public static function menu($cachePrefix = 'menu-collection-')
+    public static function menu()
     {
         $menu = '';
         if (auth()->check()) {
             $id = auth()->id();
 
-            $collection = cache()->rememberForever($cachePrefix . $id, function () {
+            $collection = cache()->rememberForever('menu-collection-' . $id, function () {
                 return self::menuForRole();
             });
 
@@ -88,7 +86,7 @@ class Menu
             session()->put('menu-selected', $route);
 
             $mc   = new MenuC;
-            $menu = $mc->getMenu($collection);
+            $menu = $mc->generarMenu($collection);
         }
 
         return $menu;
