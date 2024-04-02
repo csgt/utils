1fbc9697a7d4a09ef09d8fd9f3c4989542513372
Adrian Fulladolsa <ful21592@uvg.edu.gt>
2024-04-02 12:30:42 -0600

    fix card toggle and color

--- src/resources/views/catalogs/RoleModule.vue
+++ src/resources/views/catalogs/RoleModule.vue
@@ -1,23 +1,26 @@
 <template>
-    <div :class="color">
-        <div class="card-header">
-            {{ module.description }}
+    <div class="card">
+        <div class="card-header" :class="color">
+           <span>{{ module.description }}</span>
             <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-bs-toggle="collapse">
+                <button type="button" class="btn btn-tool" data-bs-toggle="collapse" :data-bs-target="'#collapse_body' + module.name ">
                     <i class="fas fa-minus"></i>
                 </button>
             </div>
         </div>
-        <div class="card-body">
+        <div class="card-body collapse" :id="'collapse_body' + module.name ">
             <div class="form-check" v-for="mp in module.modulepermissions" :key="mp.name">
                 <input :id="mp.name" class="form-check-input" type="checkbox" v-model="mp.enabled">
                 <label :for="mp.name" class="form-check-label">{{ mp.permission }}</label>
             </div>
         </div>
-        <div class="card-footer">
+        <div class="card-footer" >
+            <div class="mb-1">
             <a href="javascript:void(0);" @click="set(true)">Todos</a> | <a href="javascript:void(0);" @click="set(false)">Ninguno</a>
+            </div>
         </div>
     </div>
+    <br />
 </template>
 
 <script>
@@ -34,26 +37,27 @@ export default {
     },
     computed: {
         color() {
-            let clase = 'card';
+            let clase = 'card-header';
 
             if (!this.module) {
                 return clase;
             }
 
             let all = this.module.modulepermissions.reduce((carry, mp) => {
-                return carry + (!mp.module ? 1 : 0);
+                return carry + 1;
             }, 0);
 
             let selected = this.module.modulepermissions.reduce((carry, mp) => {
-                return carry + (mp.enabled && !mp.module ? 1 : 0);
+                return carry + (mp.enabled? 1 : 0);
             }, 0);
 
+
             if (all === selected) {
-                return `${clase} bg-success`;
+                return `bg-success`;
             } else if (selected === 0) {
-                return `${clase} bg-danger`;
+                return `bg-danger`;
             }
-            return `${clase} bg-warning`;
+            return `bg-warning`;
         }
     }
 }
