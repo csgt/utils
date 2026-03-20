bb0228d4b92672ba26640c308b38390a1e029a4e
Jose Galindo <jgalindo@softlogic.com.gt>
2026-03-20 13:30:30 -0600

    fix: profile controller

---
+++ src/Http/Controllers/ProfileController.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+
+class ProfileController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('component', [
+            'title'     => trans('Profile'),
+            'component' => 'profile',
+        ]);
+    }
+
+    public function detail()
+    {
+        $user = auth()->user();
+        return response()->json($user);
+    }
+
+        public function store(Request $request)
+    {
+        $rules = [
+            'user.name'     => 'required|max:255',
+            'user.email'    => [
+                'required',
+                'max:255',
+                'email',
+                Rule::unique('users', 'email')->ignore($request->user['id']),
+            ],
+        ];
+
+        if ($request->changePassword) {
+            $rules['user.password']              = 'required|min:6';
+            $rules['user.password_confirmation'] = 'required_with:user.password|same:user.password|min:6';
+        }
+
+        $request->validate($rules);
+
+        $dataRequest                = $request->user;
+        if ($request->changePassword) {
+            $dataRequest['password'] = Hash::make($request->user['password']);
+        }
+
+        $user = auth()->user();
+
+        $user->fill($dataRequest);
+        $user->save();
+
+        return response()->json('ok');
+    }
+}
--- src/resources/views/Profile.vue
+++ src/resources/views/Profile.vue
@@ -17,9 +17,17 @@
                     <div class="col-sm-12">
                         <div class="form-group">
                             <label for="nombre">Nombre</label>
-                            <input name="nombre" type="text" :class="{'form-control' : true, 'is-invalid' : validationErrors['user.name']}" v-model="data.user.name">
+                            <input
+                                name="nombre"
+                                type="text"
+                                :class="{
+                                    'form-control': true,
+                                    'is-invalid': validationErrors['user.name'],
+                                }"
+                                v-model="data.user.name"
+                            />
                             <div v-if="validationErrors['user.name']" class="invalid-feedback">
-                                {{validationErrors['user.name'][0]}}
+                                {{ validationErrors["user.name"][0] }}
                             </div>
                         </div>
                     </div>
@@ -28,7 +36,12 @@
                     <div class="col-sm-12">
                         <div class="form-group">
                             <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="active" v-model="data.changePassword">
+                                <input
+                                    type="checkbox"
+                                    class="form-check-input"
+                                    id="active"
+                                    v-model="data.changePassword"
+                                />
                                 <label class="form-check-label" for="active">Cambiar password</label>
                             </div>
                         </div>
@@ -38,16 +51,31 @@
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label for="password">Password</label>
-                            <input name="password" type="password" autocomplete="off" :class="{'form-control' : true, 'is-invalid' : validationErrors['user.password']}" v-model="data.user.password">
+                            <input
+                                name="password"
+                                type="password"
+                                autocomplete="off"
+                                :class="{
+                                    'form-control': true,
+                                    'is-invalid': validationErrors['user.password'],
+                                }"
+                                v-model="data.user.password"
+                            />
                             <div v-if="validationErrors['user.password']" class="invalid-feedback">
-                                {{validationErrors['user.password'][0]}}
+                                {{ validationErrors["user.password"][0] }}
                             </div>
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label for="password_confirmation">Repetir Password</label>
-                            <input name="password_confirmation" type="password" autocomplete="off" class="form-control" v-model="data.user.password_confirmation">
+                            <input
+                                name="password_confirmation"
+                                type="password"
+                                autocomplete="off"
+                                class="form-control"
+                                v-model="data.user.password_confirmation"
+                            />
                         </div>
                     </div>
                 </div>
@@ -80,12 +108,13 @@ export default {
         axios
             .get("/profile/0/detail")
             .then((response) => {
-                this.loading = false;
-                this.data = response.data;
+                this.data.user = response.data;
             })
             .catch((e) => {
-                this.loading = false;
                 alert(e);
+            })
+            .finally(() => {
+                this.loading = false;
             });
     },
     methods: {
