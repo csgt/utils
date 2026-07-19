e6e636255d4c3a26a48824c0bcb6eb870f605ac0
Juan Marcos Cruz Melara <juan@softlogic.com.gt>
2026-07-19 14:44:40 -0600

    test: add testbench suite for scaffolding commands and package CI workflow

---
+++ tests/Feature/ServiceProviderTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Csgt\Utils\Tests\Feature;
+
+use Csgt\Utils\Tests\TestCase;
+use Csgt\Utils\Utils;
+use Illuminate\Support\Facades\Artisan;
+
+class ServiceProviderTest extends TestCase
+{
+    public function test_the_utils_singleton_resolves()
+    {
+        $this->assertInstanceOf(Utils::class, $this->app->make('utils'));
+    }
+
+    public function test_the_scaffolding_commands_are_registered()
+    {
+        $commands = Artisan::all();
+
+        $this->assertArrayHasKey('make:csgtci', $commands);
+        $this->assertArrayHasKey('make:csgtdocs', $commands);
+        $this->assertArrayHasKey('make:csgtdocker', $commands);
+    }
+}
